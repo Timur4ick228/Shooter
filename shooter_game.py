accb22eb3adb76f4e8ba7fde6f6889ddcefb9537
Timur4ick228 <timurskomorohov023@gmail.com>
2025-04-23 20:42:28 +0500

    add scripts

--- shooter_game.py
+++ shooter_game.py
@@ -14,39 +14,67 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+def init_game():
+    global lost, score, countFire, hero, hero2, bullets, monsters, asteroids, finish
+    lost = 0
+    score = 0
+    countFire = 0
+    finish = False
+    hero = Player('mm.png', 20, 420, 10)
+    hero2 = Player('biger.png', 400, 420, 10, 5, 2)
+    bullets = sprite.Group()
+    monsters = sprite.Group()
+    for i in range(1, 5):
+        monsters.add(Enemy('cat.png', 260, 30, 3))
+    asteroids = sprite.Group()
+    for i in range(1, 3):
+        asteroids.add(Enemy2('asteroid.png', 160, 10, 3))
+
 class Player(GameSprite):
+    def __init__(self, filename, x, y, speed, life=5, type_=1, sizeX=85, sizeY=85):
+        super().__init__(filename, x, y, speed, sizeX, sizeY)
+        self.life = life
+        self.type = type_
     def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x >= 10:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x <= 600:
-            self.rect.x += self.speed
+        if self.type == 1:
+            if keys_pressed[K_a] and self.rect.x >= 10:
+                self.rect.x -= self.speed
+            if keys_pressed[K_d] and self.rect.x <= 600:
+                self.rect.x += self.speed 
+        else:
+            if keys_pressed[K_LEFT] and self.rect.x >= 10:
+                self.rect.x -= self.speed
+            if keys_pressed[K_RIGHT] and self.rect.x <= 600:
+                self.rect.x += self.speed
     def fire(self):
         global countFire
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_SPACE] and countFire > 10:
-            bullets.add(Bullet('bullet2.png', self.rect.x, self.rect.y, 5, 90, 40))
-            countFire = 0
-        countFire += 1
+        if self.type == 1:
+            if keys_pressed[K_SPACE] and countFire > 10:
+                bullets.add(Bullet('bullet2.png', self.rect.x, self.rect.y, 5, 90, 40))
+                countFire = 0
+            countFire += 1
+        else:
+            if keys_pressed[K_UP] and countFire > 30:
+                bullets.add(Bullet('bullet2.png', self.rect.x, self.rect.y, 5, 90, 40))
+                countFire = 0
+            countFire += 1
 
-class Player2(GameSprite):
-    def update(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_LEFT] and self.rect.x >= 10:
-            self.rect.x -= self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x <= 600:
-            self.rect.x += self.speed
-    def fire(self):
-        global countFire
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and countFire > 30:
-            bullets.add(Bullet('bullet2.png', self.rect.x, self.rect.y, 5, 90, 40))
-            countFire = 0
-        countFire += 1
-
-lost = 0
-score = 0
-life = 4
+# class Player2(GameSprite):
+#     def update(self):
+#         keys_pressed = key.get_pressed()
+#         if keys_pressed[K_LEFT] and self.rect.x >= 10:
+#             self.rect.x -= self.speed
+#         if keys_pressed[K_RIGHT] and self.rect.x <= 600:
+#             self.rect.x += self.speed
+#     def fire(self):
+#         global countFire
+#         keys_pressed = key.get_pressed()
+#         if keys_pressed[K_UP] and countFire > 30:
+#             bullets.add(Bullet('bullet2.png', self.rect.x, self.rect.y, 5, 90, 40))
+#             countFire = 0
+#         countFire += 1
 
 class Enemy(GameSprite):
     def update(self):
@@ -84,27 +112,15 @@ mixer.music.play()
 
 shoot = mixer.Sound('fire.ogg')
 
-hero = Player('mm.png', 20, 420, 10)
-hero2 = Player2('biger.png', 400, 420, 10)
-bullets = sprite.Group()
-monsters = sprite.Group()
-for i in range(1, 5):
-    monsters.add(Enemy('cat.png', 260, 30, 3))
-asteroids = sprite.Group()
-for i in range(1, 3):
-    asteroids.add(Enemy2('asteroid.png', 160, 10, 3))
 finish = False
 game = True
 
 font.init()
 font1 = font.SysFont('Arial', 36)
 font2 = font.SysFont('Arial', 80)
-
-
-countFire = 0
-
+init_game()
 while game:
-    #text_life = font1.render('Жизни:' + str(life), (255, 255, 255)) 
+
     text_score = font1.render('Счет:' + str(score), 1, (255, 255, 255))
     text_lose = font1.render('Пропущено:' + str(lost), 1 , (255, 255, 255))
     win = font2.render("YOU WIN", True, (0, 255, 0))
@@ -112,12 +128,15 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        
+        if e.type == KEYDOWN:
+            if e.key == K_r:
+                init_game() 
     if not finish:
 
         window.blit(background,(0,0))
         window.blit(text_lose, (0, 35))
         window.blit(text_score, (0, 5))
-        #window.blit(text_life, (0, 500))
         monsters.draw(window)
         asteroids.draw(window)
         bullets.draw(window)
@@ -137,9 +156,9 @@ while game:
             monsters.add(monster)
             if sprite.spritecollide(hero, monsters, False) or sprite.spritecollide(hero2, monsters, False):
                 sprite.spritecollide(hero, monsters, True) or sprite.spritecollide(hero2, monsters, True)
-                life = life - 1
+                
 
-        if lost >= 10 or life <= 0:
+        if lost >= 10 or hero.life <= 0:
             finish = True
             window.blit(lose, (200, 200))
             
